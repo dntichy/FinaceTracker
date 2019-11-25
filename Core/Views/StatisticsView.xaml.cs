d0dc18639ddddfbbf16bb1a3a9330ed81307b555
dtichy <dntichy@gmail.com>
2019-11-25 07:30:45 +0100

    chart extension

--- Core/Views/StatisticsView.xaml.cs
+++ Core/Views/StatisticsView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Core.Models;
+using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +24,71 @@ namespace Core.Views
     /// </summary>
     public partial class StatisticsView : UserControl
     {
+        public Func<double, string> FormatterForY { get; set; }
+
         public StatisticsView()
         {
             InitializeComponent();
+            DataContext = this;
+
+            FormatterForY = value => value + " €";
+
+
+
+            //todo fill in the empty dates
+            DateTime MinDate = new DateTime(2019, 10, 1);//todo input possibility 
+            DateTime MaxDate = new DateTime(2019, 10, 31);
+
+            var TransactionRepo = new TransactionRecordRepository();
+            //var aggreg = TransactionRepo.TxRepository
+            //    .Where(n => n.Date > MinDate).Where(n => n.Date < MaxDate)
+            //    .GroupBy(n => n.Date.GetValueOrDefault().Day)
+            //    .Select(g => g.Sum(x => x.Amount));
+
+            var testAggr = Enumerable.Range(0, 31)
+        .Select(a => MinDate.AddDays(a))
+        .Where(n => n.Date >= MinDate).Where(n => n.Date <= MaxDate)
+        .GroupJoin(TransactionRepo.TxRepository,
+        outer => outer,
+        inner => inner.Date,
+        (outer, group) => new XYAxisHelper
+        {
+            x = outer.Day,
+            y = group.Sum(n => n.Amount)
+        });
+
+
+            Console.WriteLine(testAggr);
+
+            var dayConfig = Mappers.Xy<XYAxisHelper>()
+                          .X(dayModel => dayModel.x)
+                          .Y(dayModel => dayModel.y);
+
+            SeriesCollection = new SeriesCollection(dayConfig) {
+                new LineSeries {
+                    Values = new ChartValues<XYAxisHelper>(testAggr)
+                }
+            };
+
+            TestChart.AxisX = new AxesCollection()
+{
+                new Axis()
+                {
+                    Title = "Day of month",
+                    Separator = new LiveCharts.Wpf.Separator()
+                    {
+                        Step = 1.0,
+                        IsEnabled = false
+                    }
+
+                }
+};
+        }
+
+        public SeriesCollection SeriesCollection
+        {
+            get;
+            private set;
         }
     }
 }
---
+++ Core/Views/XYAxisHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Views
+{
+    internal class XYAxisHelper
+    {
+        public int x { get; set; }
+        public float y { get; set; }
+    }
+}
