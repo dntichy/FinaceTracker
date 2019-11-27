a2b70e106da7a452e2c3c75df6a2c58e39b760bd
dtichy <dntichy@gmail.com>
2019-11-27 22:13:06 +0100

    statistics enhanced, binding to cboxes, dynamic dates, refactored

---
+++ Core/Views/PieChartCategoryWrapper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Views
+{
+    internal class PieChartCategoryWrapper
+    {
+        public string category { get; set; }
+        public float sumAmounts { get; set; }
+        public IEnumerable<string> color { get; set; }
+    }
+}
--- Core/Views/StatisticsView.xaml.cs
+++ Core/Views/StatisticsView.xaml.cs
@@ -4,6 +4,7 @@ using LiveCharts.Configurations;
 using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,48 +26,103 @@ namespace Core.Views
     public partial class StatisticsView : UserControl
     {
         public Func<double, string> FormatterForY { get; set; }
+        TransactionRecordRepository TransactionRepo = new TransactionRecordRepository();
+       
 
         public StatisticsView()
         {
             InitializeComponent();
             DataContext = this;
 
+            PrepareMonthCombobox();
+            PrepareYearsCombobox();
+
             FormatterForY = value => value + " €";
 
 
+            var month = monthsCombobox.Text;
+            var year = int.Parse(yearComboBox.Text);
+            Enum.TryParse(month, true, out Months mnth);
 
-            //todo fill in the empty dates
-            DateTime MinDate = new DateTime(2019, 10, 1);//todo input possibility 
-            DateTime MaxDate = new DateTime(2019, 10, 31);
+            DateTime MinDate = new DateTime(year, (int)mnth, 1);
 
-            var TransactionRepo = new TransactionRecordRepository();
-            //var aggreg = TransactionRepo.TxRepository
-            //    .Where(n => n.Date > MinDate).Where(n => n.Date < MaxDate)
-            //    .GroupBy(n => n.Date.GetValueOrDefault().Day)
-            //    .Select(g => g.Sum(x => x.Amount));
+            var totalSpentInGivenInterval = Enumerable.Range(0, DateTime.DaysInMonth(year, (int)mnth)-1)
+            .Select(a => MinDate.AddDays(a))
+            .Where(n => GetMonthNameFromMonth(n.Date.Month) == month).Where(n => n.Date.Year == year)
+            .GroupJoin(TransactionRepo.TxRepository,
+            outer => outer,
+            inner => inner.Date,
+            (outer, group) => new XYAxisHelper
+            {
+                x = outer.Day,
+                y = group.Sum(n => n.Amount)
+            });
 
-            var testAggr = Enumerable.Range(0, 31)
-        .Select(a => MinDate.AddDays(a))
-        .Where(n => n.Date >= MinDate).Where(n => n.Date <= MaxDate)
-        .GroupJoin(TransactionRepo.TxRepository,
-        outer => outer,
-        inner => inner.Date,
-        (outer, group) => new XYAxisHelper
+
+            var amountPerCategories = TransactionRepo.TxRepository
+             .Where(n => n.Date >= MinDate)
+             .GroupBy(n => n.Category.Name)
+             .Select(group => new PieChartCategoryWrapper
+             {
+                 category = group.Key,
+                 sumAmounts = group.Sum(x => x.Amount),
+                 color = group.Select(n => n.Category.Color)
+             });
+
+
+            CreateLineChart(totalSpentInGivenInterval);
+            CreateCollumnChart(totalSpentInGivenInterval);
+            CreatePieChart(amountPerCategories);
+        }
+
+
+        private string GetMonthNameFromMonth(int month)
         {
-            x = outer.Day,
-            y = group.Sum(n => n.Amount)
-        });
+            return CultureInfo.InvariantCulture.DateTimeFormat.MonthNames[month - 1];
+        }
+
+        private void CreatePieChart(IEnumerable<PieChartCategoryWrapper> amountPerCategories)
+        {
+            Func<ChartPoint, string> labelPoint = chartPoint =>
+             string.Format("{0:n0} ({1:P})", chartPoint.Y, chartPoint.Participation);
 
 
-            Console.WriteLine(testAggr);
+            PieChart.Series = new SeriesCollection(amountPerCategories);
 
-            var dayConfig = Mappers.Xy<XYAxisHelper>()
-                          .X(dayModel => dayModel.x)
-                          .Y(dayModel => dayModel.y);
+            foreach (var item in amountPerCategories)
+            {
+                PieSeries pie = new PieSeries();
+                pie.Title = item.category;
+                pie.Values = new ChartValues<float> { item.sumAmounts };
+                pie.LabelPoint = labelPoint;
+                pie.DataLabels = true;
+                PieChart.Series.Add(pie);
+            }
+            PieChart.LegendLocation = LegendLocation.Right;
+        }
 
+        private void CreateCollumnChart(IEnumerable<XYAxisHelper> totalSpentInGivenInterval)
+        {
+            CartesianMapper<XYAxisHelper> dayConfig = Mappers
+              .Xy<XYAxisHelper>()
+                        .X(model => model.x)
+                        .Y(model => model.y);
+            SeriesCollection2 = new SeriesCollection(dayConfig) {
+                new ColumnSeries {
+                    Values = new ChartValues<XYAxisHelper>(totalSpentInGivenInterval)
+                }
+            };
+        }
+
+        private void CreateLineChart(IEnumerable<XYAxisHelper> totalSpentInGivenInterval)
+        {
+            CartesianMapper<XYAxisHelper> dayConfig = Mappers
+              .Xy<XYAxisHelper>()
+                        .X(model => model.x)
+                        .Y(model => model.y);
             SeriesCollection = new SeriesCollection(dayConfig) {
                 new LineSeries {
-                    Values = new ChartValues<XYAxisHelper>(testAggr)
+                    Values = new ChartValues<XYAxisHelper>(totalSpentInGivenInterval)
                 }
             };
 
@@ -83,22 +139,25 @@ namespace Core.Views
 
                 }
 };
-            SeriesCollection2 = new SeriesCollection(dayConfig) {
-                new ColumnSeries {
-                    Values = new ChartValues<XYAxisHelper>(testAggr)
-                }
-            };
+        }
 
-            Func<ChartPoint, string> labelPoint = chartPoint =>
-     string.Format("{0:n0} ({1:P})", chartPoint.Y, chartPoint.Participation);
-            PieChart.Series = new SeriesCollection();
-            PieSeries pie = new PieSeries();
-            pie.Title = "Blue";
-            pie.Values = new ChartValues<double> { Convert.ToDouble(10) };
-            pie.LabelPoint = labelPoint;
-            pie.DataLabels = true;
-            PieChart.Series.Add(pie);
+        private void PrepareYearsCombobox()
+        {
+            yearComboBox.ItemsSource = Enumerable.Range(2018, DateTime.Now.Year - 2018 + 1).ToList();
+            yearComboBox.SelectedItem = DateTime.Now.Year;
+        }
 
+        private void RefreshChartsAccordingCombBoxes()
+        {
+
+        }
+
+        private void PrepareMonthCombobox()
+        {
+            monthsCombobox.ItemsSource = CultureInfo.InvariantCulture.DateTimeFormat
+                                                     .MonthNames.Take(12).ToList();
+            monthsCombobox.SelectedItem = CultureInfo.InvariantCulture.DateTimeFormat
+                                                    .MonthNames[DateTime.Now.AddMonths(-1).Month - 1];
         }
 
         public SeriesCollection SeriesCollection
@@ -111,6 +170,6 @@ namespace Core.Views
             get;
             private set;
         }
-     
+
     }
 }
